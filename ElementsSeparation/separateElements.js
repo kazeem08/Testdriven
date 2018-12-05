0c022cfce7b47ea49333eaabdeaaf4d5113b7fdc
Tyav <tyav2greenz@gmail.com>
2018-12-05 17:18:56 +0100

    solved

--- ElementsSeparation/separateElements.js
+++ ElementsSeparation/separateElements.js
@@ -1,13 +1,13 @@
 function separateElements(array){
 	var arrayArranged = [[],[]]
     for (i = 0; i < array.length; i++){
-		if (isNaN(array[i])){
+		if (typeof array[i] !== 'number'){
 			arrayArranged[0].push(array[i]);
     	} else {
 			arrayArranged[1].push(Math.floor(array[i]));
         }
 	}
-	return (arrayArranged[0].sort((a, b) => a - b).concat(arrayArranged[1].sort()));
+	return arrayArranged[0].sort((a,b)=> a - b).concat(arrayArranged[1].sort());
 }
 
 
