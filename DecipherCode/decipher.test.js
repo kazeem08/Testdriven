f90c032d792fd5e38464cbe26dedc5ecfe4ffd5c
Oguazu Stanley <soguazu@gmail.com>
2018-12-06 12:41:12 +0100

    Completed the test cases

--- DecipherCode/decipher.test.js
+++ DecipherCode/decipher.test.js
@@ -22,4 +22,23 @@ test('Decipher expects FM6KGrCBjj and return SZ6XTEPOWW', () => {
     expect(decipher('FM6KGrCBjj')).toEqual('SZ6XTEPOWW');
 })
 
+test('Decipher expects FM6KGrCBjj and return SZ6XTEPOWW', () => {
+    expect(decipher('FM6KGrCBjj')).toEqual('SZ6XTEPOWW');
+})
+
+test('GUR DHVPX OEBJA SBK WHZCRQ BIRE GUR YNML QBT and return THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG', () => {
+    expect(decipher('GUR DHVPX OEBJA SBK WHZCRQ BIRE GUR YNML QBT')).toEqual('THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG');
+})
+
+test("expect RNG NG WBR'F NG 1:00CZ and return EAT AT JOE\'S AT 1:00PM", () => {
+    expect(decipher("RNG NG WBR'F NG 1:00CZ")).toEqual('EAT AT JOE\'S AT 1:00PM');
+})
+
+test("expect LNL!!! to return YAY!!!", () => {
+    expect(decipher("LNL!!!")).toEqual('YAY!!!');
+})
+
+test("1234567890 to return 1234567890", () => {
+    expect(decipher("1234567890")).toEqual('1234567890');
+})
 
