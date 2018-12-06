96c7be7710056f6a140f9a5b185508d738cc02fd
Oguazu Stanley <soguazu@gmail.com>
2018-12-06 12:56:36 +0100

    Completed the test cases

--- DecipherCode/decipher.js
+++ DecipherCode/decipher.js
@@ -1,21 +1,5 @@
 function decipher(str) { 
-     str  = str.toUpperCase();
-  return str.split("")
-            .map.call(str, function(charVar) {
-            x = charVar.charCodeAt(0);
-            
-            if(x < 65 || x > 90) {
-              return String.fromCharCode(x);
-            }
-            
-            else if(x < 78) {
-              return String.fromCharCode(x + 13);
-            }
-            
-            else {
-              return String.fromCharCode(x - 13);
-            }
-            }).join("");
+    
    
 }
 
